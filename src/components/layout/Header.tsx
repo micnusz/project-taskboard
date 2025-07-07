c6eab9a99fe61d0d9a09a7f051dc244b7e1e48e4
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-07-07 20:54:17 +0200

    Changed header ui

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,20 @@
 "use client";
+import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="px-fluid py-4 border-b-2">
-      <nav>
+      <nav className="flex flex-row justify-between">
         <div>
+          <Link
+            href={"/"}
+            className="text-foreground hover:text-muted-foreground"
+          >
+            <h1>Project Taskboard</h1>
+          </Link>
+        </div>
+        <div className="flex justify-end">
           <a
             href="https://github.com/micnusz/project-taskboard"
             target="_blank"
@@ -13,10 +22,7 @@ const Header = () => {
             aria-label="Go to github repository."
             className="text-foreground hover:text-muted-foreground "
           >
-            <div className="flex flex-row gap-x-4 hover:text-chart-3">
-              <FaGithub className="w-6 h-6" />
-              <h1>Project Taskboard</h1>
-            </div>
+            <FaGithub className="w-6 h-6" />
           </a>
         </div>
       </nav>
