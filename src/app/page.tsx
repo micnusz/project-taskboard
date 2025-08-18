2640d655ed414b59e1e713e35fd41995b8fd9a6f
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-08-18 10:40:37 +0200

    Removed prefetching queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 "use server";
 
-import { getTaskCount, getAuthors, searchTask } from "@/actions/actions";
 import HomeClientPage from "@/components/pages/Home";
-import { getQueryClient } from "@/lib/get-query-client";
-import Spinner from "@/lib/Spinner";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Metadata } from "next";
-import { Suspense } from "react";
 
 export const generateMetadata = async (): Promise<Metadata> => {
   return {
@@ -16,67 +11,5 @@ export const generateMetadata = async (): Promise<Metadata> => {
 };
 
 export default async function Home() {
-  const queryText = "";
-  const offset = 0;
-  const limit = 10;
-  const priority = undefined;
-  const status = undefined;
-  const type = undefined;
-  const date = undefined;
-  const sortField = "createdAt";
-  const sortOrder = "desc";
-  const author = undefined;
-
-  const queryClient = getQueryClient();
-
-  //Prefetch users
-  await queryClient.prefetchQuery({
-    queryKey: ["users"],
-    queryFn: () => getAuthors(),
-  });
-
-  //Prefetch task-count
-  await queryClient.prefetchQuery({
-    queryKey: ["task-count", queryText, priority, status, type, date, author],
-    queryFn: () =>
-      getTaskCount(queryText, priority, status, type, date, author),
-  });
-
-  //Prefetch tasks
-  await queryClient.prefetchQuery({
-    queryKey: [
-      "tasks",
-      queryText,
-      limit,
-      offset,
-      priority,
-      status,
-      type,
-      date,
-      sortField,
-      sortOrder,
-      author,
-    ],
-    queryFn: () =>
-      searchTask(
-        queryText,
-        limit,
-        offset,
-        priority,
-        status,
-        type,
-        date,
-        sortField,
-        sortOrder,
-        author
-      ),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<Spinner />}>
-        <HomeClientPage />
-      </Suspense>
-    </HydrationBoundary>
-  );
+  return <HomeClientPage />;
 }
